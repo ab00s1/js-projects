98ebeda6f0a81fa6d1f3c23930ee2cfa741d444f
Abhinav Singh <157624328+ab00s1@users.noreply.github.com>
2024-10-02 12:36:06 +0530

    scroll-to-top-script.js

--- 03_foodie-hamburger/final-code/script.js
+++ 03_foodie-hamburger/final-code/script.js
@@ -3,6 +3,11 @@ const headerContent = document.querySelector('.header-content')
 const closeIcon = document.querySelector('.close-icon')
 const nav = document.querySelector('nav')
 
+document.querySelector("body > div.main-content > main > footer > h3 > a").addEventListener("click", function() {
+  window.scrollTo(0, 0);
+});
+
+
 hamburgerIcon.addEventListener('click', (e) => {
     e.stopPropagation()
     headerContent.classList.add('menu-open')
